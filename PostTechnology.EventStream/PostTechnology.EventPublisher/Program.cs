bb81bc80f92bfb7ade282be20cc80c6326e8aedc
Владимир Лыков <dirplex@gmail.com>
2020-09-14 12:12:02 +0400

    Тестовый проект Publisher-Subscriber

---
+++ PostTechnology.EventStream/PostTechnology.EventPublisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using STAN.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PostTechnology.EventPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clusterId = "test-cluster";
+            var clientId = $"producer-{Guid.NewGuid().ToString("N")}";
+            var subject = "PostTechnology.EventBus";
+
+            //var options = StanOptions.GetDefaultOptions();
+            //options.NatsURL = "nats://localhost:4222";
+
+            using (var cn = new StanConnectionFactory().CreateConnection(clusterId, clientId))
+            {
+                var cts = new CancellationTokenSource();
+
+                Task.Run(async () =>
+                {
+                    var rnd = new Random();
+
+                    while (!cts.IsCancellationRequested)
+                    {
+                        cn.Publish(subject, BitConverter.GetBytes(rnd.Next(-10, 40)));
+
+                        await Task.Delay(1000, cts.Token);
+                    }
+                }, cts.Token);
+
+                Console.WriteLine("Hit any key to exit");
+                Console.ReadKey();
+                cts.Cancel();
+            }
+        }
+    }
+}
---
+++ PostTechnology.EventStream/PostTechnology.EventSubscriber/Program.cs
@@ -0,0 +1,43 @@
+﻿using STAN.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PostTechnology.EventSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clusterId = "test-cluster";
+            var clientId = $"consumer-AppName";
+            var subject = "PostTechnology.EventBus";
+
+            var opts = StanSubscriptionOptions.GetDefaultOptions();
+            opts.DurableName = "PostTechnology.Durable";
+            IStanSubscription subscription = null;
+
+            //var options = StanOptions.GetDefaultOptions();
+            //options.NatsURL = "nats://localhost:4222";
+
+            using (var cn = new StanConnectionFactory().CreateConnection(clusterId, clientId))
+            {
+                var cts = new CancellationTokenSource();
+
+                Task.Run(() =>
+                {
+                    subscription = cn.Subscribe(subject, opts, (obj, args) =>
+                    {
+                        var t = BitConverter.ToInt32(args.Message.Data, 0);
+                        Console.WriteLine($"{t}C");
+                    });
+                }, cts.Token);
+
+                Console.WriteLine("Hit any key to exit");
+                Console.ReadKey();
+                subscription.Close();
+                cts.Cancel();
+            }
+        }
+    }
+}
